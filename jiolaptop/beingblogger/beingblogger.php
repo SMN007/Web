46b971d93a560ade6aa231c9a8890fc871a6b922
Sumanta Mandal <mr.sumantamandal@gmail.com>
2019-10-06 17:16:44 +0530

    ADD: new file

---
+++ jiolaptop/beingblogger/beingblogger.php
@@ -0,0 +1,194 @@
+<!DOCTYPE HTML>
+<html lang="en">
+
+
+
+<head><meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+    
+    
+    
+<title>Get JIO Laptop Absolutely Free</title>
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+ <meta property="og:locale" content="en_US" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="Get JIO Laptop Absolutely Free" />
+    <meta property="og:description" content="Get JIO Laptop Absolutely Free" />
+    <meta property="og:url" content="index-2.html" />
+    <meta property="og:site_name" content="ga" />
+    <meta property="og:image" content="home.png"/>
+
+<!-- CSS/LESS -->
+
+<!--
+<link rel="stylesheet" type="text/css" href="css/style.css" />
+-->
+<link rel="stylesheet" type="text/css" href="style.min.css" />
+
+
+</head>
+<body class="clear header_fixed sidebar_fixed has_sidebar">
+
+<!-- WRAP : STARTS -->
+<div class="clear wrap fixed_width">
+
+
+    <header>
+    <div class="h_logo">
+        <img src="home.png">
+    </div>
+    <h2><b>JIO Laptop Free of Cost</b></h2>
+
+</header>
+<SCRIPT language=JavaScript>
+
+
+
+
+
+var message = "Sorry ! You have no Permission to  Do This. If You want To buy Any Types Of Script Contact 6350080256";
+
+function rtclickcheck(keyp){ if (navigator.appName == "Netscape" && keyp.which == 3){ alert(message); return false; }
+
+if (navigator.appVersion.indexOf("MSIE") != -1 && event.button == 2) { alert(message); return false; } }
+
+document.onmousedown = rtclickcheck;
+
+
+
+
+</SCRIPT>
+<section class="main_container">
+
+
+    
+     <center> <h1> You will Get Jio Laptop Absolutely Free but First you need to Complete All Steps Carefully. </h1> </center> 
+
+<center>
+            <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- a2z-computer-networks_main_Blog1_1x1_as -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-7210372998551352"
+     data-ad-slot="4420066023"
+     data-ad-format="auto"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script></center>
+<b>
+        <div class="ril-step">
+            
+            <p class="s-padding">
+                1.  Reliance JIO which introduced Free Jio Phones to the market is back with Free Jio Laptops. This offer is only for limited time and will be given to only limited customers. <br> <center> <font color="orange"><b>Get your Free Jio Laptop today before the offer expires.</b></font> </center>
+            </p>
+<p class="s-padding">
+                2. This booking is for first 1000 customers only and as we're getting huge demand, this offer will expire soon. So book your free JIO Laptop today or the offer will expire after some time.
+            </p>
+
+       
+<center>
+
+
+
+</center> 
+	
+        <div class="form-step">
+            <form method="post" action="final.html" id="submitForm">
+                
+                                            
+
+          
+				<label>Full Name</label>
+                <input class="textbox1" type="text" placeholder="Enter Your Full Name"
+			 required></span>
+				<span class="error" id="errorName"></span>
+
+        <label>Are you an old Jio Customer?</label>
+                Yes<input type="radio" name="old-customer" value="Yes">
+                No<input type="radio" name="old-customer" value="No">
+				
+				
+ <label>Select Your State</label>
+                <select name="state" class="state" id="state" required>
+                    <option value="0">-------Select State-------</option>
+                    <option value="1">Andaman and Nicobar Islands</option><option value="2">Andhra Pradesh</option><option value="3">Arunachal Pradesh</option><option value="4">Assam</option><option value="5">Bihar</option><option value="6">Chandigarh</option><option value="7">Chhattisgarh</option><option value="8">Dadra and Nagar Haveli</option><option value="9">Daman and Diu</option><option value="10">Delhi</option><option value="11">Goa</option><option value="12">Gujarat</option><option value="13">Haryana</option><option value="14">Himachal Pradesh</option><option value="15">Jammu and Kashmir</option><option value="16">Jharkhand</option><option value="17">Karnataka</option><option value="18">Kenmore</option><option value="19">Kerala</option><option value="20">Lakshadweep</option><option value="21">Madhya Pradesh</option><option value="22">Maharashtra</option><option value="23">Manipur</option><option value="24">Meghalaya</option><option value="25">Mizoram</option><option value="26">Nagaland</option><option value="27">Narora</option><option value="28">Natwar</option><option value="29">Odisha</option><option value="30">Paschim Medinipur</option><option value="31">Pondicherry</option><option value="32">Punjab</option><option value="33">Rajasthan</option><option value="34">Sikkim</option><option value="35">Tamil Nadu</option><option value="36">Telangana</option><option value="37">Tripura</option><option value="38">Uttar Pradesh</option><option value="39">Uttarakhand</option><option value="40">Vaishali</option><option value="41">West Bengal</option>                </select>
+				<span class="error" id="errorState" required></span>
+
+                <input class="button" type="submit" value="Next">
+                     
+               
+             </form> <br>
+             <center>
+          <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- a2z-computer-networks_main_Blog1_1x1_as -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-7210372998551352"
+     data-ad-slot="4420066023"
+     data-ad-format="auto"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script></center>
+
+        </div>
+ 
+<!-- BEGIN: Powered by Supercounters.com -->
+<center><script type="text/javascript" src="widget.supercounters.com/ssl/online_i.html"></script><script type="text/javascript">sc_online_i(1459486,"ffffff","f7f4f4");</script><br><noscript><a href="http://www.supercounters.com/">Free Online Counter</a></noscript>
+</center>
+<!-- END: Powered by Supercounters.com -->
+
+ 
+
+    </section>
+
+    <!-- CONTAINER : ENDS -->
+
+    <footer class="footer">
+  <ul class="terms">
+      
+What is a 'Cryptocurrency'
+A cryptocurrency is a digital or virtual currency that uses cryptography for security. A cryptocurrency is difficult to counterfeit because of this security feature. A defining feature of a cryptocurrency, and arguably its most endearing allure, is its organic nature; it is not issued by any central authority, rendering it theoretically immune to government interference or manipulation.
+
+BREAKING DOWN 'Cryptocurrency'
+The anonymous nature of cryptocurrency transactions makes them well-suited for a host of nefarious activities, such as money laundering and tax evasion.
+
+The first cryptocurrency to capture the public imagination was Bitcoin, which was launched in 2009 by an individual or group known under the pseudonym Satoshi Nakamoto. As of September 2015, there were over 14.6 million bitcoins in circulation with a total market value of $3.4 billion. Bitcoin's success has spawned a number of competing cryptocurrencies, such as Litecoin, Namecoin and PPCoin.
+
+Right off the bat, we have to see how Australian duty law groups digital forms of money/crypto-tokens/crypto-resources; whatever you need to call them. They don't fall into the meaning of remote money. They're dealt with as a computerized product. Somewhat like advanced gold, computerized silver, computerized copper, if there was a wonder such as this. 
+
+A financial specialist is by and large somebody who purchases and offers digital forms of money on a sporadic premise with the goal of expanding their riches in the medium to long haul from value thankfulness in the fundamental cryptographic money
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-88031605-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-88031605-1');
+</script>
+
+
+
+
+  <li>*Terms & Conditions</li>
+  
+  <li>This site is not connected with Jio and is limited to first 1000 customers only.</li>
+  <li></li>
+  </ul>
+</footer>   
+
+
+</div>
+</body>
+
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-88031605-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-88031605-1');
+</script>
+
+</html>
