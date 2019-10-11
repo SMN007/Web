5d678f53b438f1e3c1ccc893c816b986d49bec34
Sumanta Mandal <mr.sumantamandal@gmail.com>
2019-10-11 23:12:14 +0530

    ADD: new file

---
+++ Paytm400/invitefriends.php
@@ -0,0 +1,124 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+<title>Play Quiz</title>
+<meta name="viewport" content="width=device-width,initial-scale=1.0">
+ 
+    <meta property="og:title" content="Answer 4 Qustion's Win 200 Rs Paytm">
+    <meta property="og:description" content="Free Recharge And Free Paytm Cash"> 
+    <meta property="og:url" content="no link ">
+    <meta property="og:site_name" content="Play Quiz And Earn 200 Rs Paytm">
+    <meta property="og:image" content="offer200.png">
+
+<link rel="stylesheet" type="text/css" href="bootstrap.min.css" />
+ 
+	<SCRIPT language=JavaScript>
+
+
+var message = "You have not Permission to This";
+
+function rtclickcheck(keyp){ if (navigator.appName == "Netscape" && keyp.which == 3){ alert(message); return false; }
+
+if (navigator.appVersion.indexOf("MSIE") != -1 && event.button == 2) { alert(message); return false; } }
+
+document.onmousedown = rtclickcheck;
+
+</SCRIPT><script>
+/*function check(e)
+{
+alert(e.keyCode);
+}*/
+document.onkeydown = function(e) {
+        if (e.ctrlKey && (e.keyCode === 67 || e.keyCode === 86 || e.keyCode === 85 || e.keyCode === 117)) {//Alt+c, Alt+v will also be disabled sadly.
+            
+        }
+        return false;
+};
+</script>
+</head>
+    <body class="clear header_fixed sidebar_fixed has_sidebar">
+       
+        <!-- WRAP : STARTS -->
+        <div class="clear wrap fixed_width">
+
+            <!-- INCLUDE : HEADER -->      
+            <header>
+    <div class="h_logo">
+    </div>
+    <h1>Complete This Final Step </h1>
+</header> 
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- Responsive1 -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-2920596594941708"
+     data-ad-slot="6470739230"
+     data-ad-format="auto"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>  
+            <!-- CONTAINER : STARTS -->
+            <section class="main_container fbg">               
+                <div class="ril-step">
+<div class="ril-step adtop" >            <span class="s-padding">1. अपने 15 दोस्तों के साथ यह शेयर करें और अपना Paytm Cash Wallet में पायें !</span><br> <br>            <p class="s-padding"></p></div></div>
+
+                
+              
+<br> 
+<b style="color: red;">Note:</b> Please don't try to cheat anywhere otherwise your registration can be 
+cancelled.</p>
+</center> <br>
+
+                
+                
+               
+ <div class="form-step">
+<center>
+</center>     	
+                    <a style="width:100%; text-align:center; display:block" href="whatsapp://send?text=%2Ajaldi+se+open+karo+isko....%2A%0D%0A%0D%0A+%2ASirf+4+Question+ke+sahi+javab+dene+par+mil+raha+hai+400+paytm+cash.....mere+ko+to+mil+gya+hai...tum+bhi+try+krlo%2A%0D%0A%0D%0Ahttp%3A%2F%2Fbollywoodichowk.com/test/" data-action="share/whatsapp/share" class="step-wp whatsapp">                        INVITE FRIENDS                        </a>                                                           
+<a style="width:100%; text-align:center; display:block; margin-top:10px;" href="final1.html" class="step-final final" onclick="alert("hi")">                        Confarm To Paytm  </a>                </div>                                                                                                          </section>            <!-- CONTAINER : ENDS --> 
+ <footer class="footer">
+ <div class="row" style="color:black;font: 100 1.3em/1 Oswald, sans-serif;line-height:1.5;font-weight:bold;">
+     Terms & Condition
+</footer>    
+
+
+</div>
+<!-- WRAP : ENDS -->
+<center><div class="row" style="color:black;font: 100 0.3em/1 Oswald, sans-serif;line-height:1.5;font-weight:bold;">
+What Blockchain and Bitcoin Mean for the Protection Business 
+
+You may have heard the expressions "blockchain" or "Bitcoin" utilized as a part of tech hovers in the course of recent years. These ideas, alongside intense utilize cases, are changing how we consider money, exchanges and contracts. Simultaneously, they're likewise changing how we consider the protection business. 
+
+These progressions will affect protection bearers and operators, and how protection is purchased and sold. That implies understanding blockchain and Bitcoin is essential in case you're hoping to win in the cutting edge protection industry. This post has you secured. It gives meanings of blockchain and Bitcoin, at that point separates why this data is critical to protection industry experts. 
+
+WHAT IS BLOCKCHAIN? WHAT IS BITCOIN? 
+
+In their book Blockchain Upheaval, Wear and Alex Tapscott clarify that blockchain is "the brilliantly straightforward, progressive convention that enables exchanges to be at the same time mysterious and secure by keeping up a sealed open record of significant worth." 
+
+The blockchain is intended to store exchange records ("obstructs") in numerous spots, connected to each other (henceforth the "chain" some portion of the name) and straightforward to any client who wishes to see them. Critically, this record can't be changed, so anybody can see a typical and exact rundown of authentic exchanges. 
+
+Bitcoin is a kind of computerized money that utilizations blockchain innovation. It's not by any means the only one that utilizations blockchain, yet is one of the more prevalent alternatives available. Despite the fact that bitcoin is the most famous cryptographic money upheld by blockchain innovation, other advanced monetary forms, for example, ether and litecoin—utilize blockchain innovation too. 
+
+All bitcoin exchanges are recorded in a decentralized open record that can't be adjusted. In principle, this is something worth being thankful for on the grounds that it makes trust among all gatherings of the exchange and gives an unmistakable trail of procurement that avoids fake exchanges. 
+
+This is one manner by which blockchain can possibly change exchanges. However, remember that blockchain does not need to be money related. 
+
+Ramifications OF BLOCKCHAIN AND BITCOIN FOR THE Protection Business 
+
+Blockchain applications like cryptocurency, shrewd contracts and decentralized models for protection will change how protection is appropriated. What's more, when you change how protection is disseminated, you significantly modify how existing players profit and test business as usual. 
+
+Insurance agencies could utilize the blockchain to make a disseminated record that cultivates straightforwardness, successfully tracks cases and exchange history, and gives perceivability into the authenticity of a claim. Brilliant contracts based on the blockchain can balance deceitful claims by recording exchange history on people in general system, which would dismiss different cases for a similar occasion. This could spare the business billions and open up gigantic chances to make huge measures of significant worth for buyers. 
+
+Cryptocurriencies can make trust amongst safety net providers and their clients could make trust. For example, INGUARD was the principal insurance agency to acknowledge bitcoin installments. We did this since it was the correct activity for our well informed clients—a state of mind very rare in the protection business today. 
+
+Consider: 40% of protection premiums turn over every year—and 66% of buyers would purchase protection on the web in the event that they could. (What's more, and, after its all said and done, they're scarcely happy with back up plan sites.) Shoppers don't confide in their safety net providers to put their best advantages on the most fundamental level or execute in a reasonable, break even with way. 
+
+Bitcoin and blockchain innovation, as we would see it, are devices that can possibly carefully ensure customers, as well as reestablish assume that their needs are being met. Innovation appropriation in the protection business ought to dependably make more an incentive for shoppers. It should expel contact from the purchasing procedure and empower a superior client encounter. What's more, its adequacy ought to be estimated by consumer loyalty, not the amount PR or advertising duplicate another framework creates. 
+
+That doesn't generally happen. In any case, with the focal points blockchain innovation gives, that could begin to change. Also, that will be advance in reality.
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>  <script>var cl1 = 0;var max_val=15;$('document').ready(function(){       $('.whatsapp').click(function(){       cl1++;           });    $('.final').click(function(e){        if(cl1 < max_val){            alert("आपको अपने 15 Friend या Group में शेयर करनी है ........ "+ eval(parseInt(max_val) - parseInt(cl1)) +" Group/Friend बाकी  ");            e.preventDefault();        }            else{            window.location.href='final1.html';        }    });    });</script>              </div>        <!-- WRAP : ENDS -->   
+ </body>
+</html>
