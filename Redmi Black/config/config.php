bee5312163a40b87750a6242039b0e01a5150ed3
Sumanta Mandal <mr.sumantamandal@gmail.com>
2019-10-13 17:34:44 +0530

    ADD:New files

---
+++ Redmi Black/config/config.php
@@ -0,0 +1,499 @@
+
+
+
+
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+<head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-53259372-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-53259372-1');
+</script>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta property="og:locale" content="en_US" />
+    <META NAME="ROBOTS" CONTENT="NOINDEX, NOFOLLOW">
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="Redmi Note 5 Pro Flash Sale In 699 rs" />
+<meta property="og:site_name" content="Redmi Note 5 Pro Sale">
+<meta property="og:description" content="Redmi Note 5 Pro Flash Sale Started at Just 699rs. This Offer only for 100 Lucky Customers Daily" />
+<meta property="og:image" content="img/1.jpg"/>
+<meta property="og:type" content="website" />
+    <title>Redmi Note 5 Pro Sale</title>
+    <link rel="stylesheet" href="bootstrap.min.css">
+   <script src="bootstrap.min.js" integrity="sha384-a5N7Y/aK3qNeh15eJKGWxsqtnX/wWdSZSKp+81YjTmS15nvnvxKHuzaWwXHDli+4" crossorigin="anonymous"></script>
+
+    </script>
+     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <style>
+html, body {
+    max-width: 100%;
+    overflow-x: hidden;
+}
+        *{
+            margin: 0px;
+            padding: 0px;
+            font-size: 13px !important;
+        }
+        body{
+              overflow-x: hidden;
+  overflow-y: scroll;
+        }
+        .logo{
+
+            background-size: 225px !important;
+        }
+		
+        .stars{
+            float: left;
+        }
+		.cart{
+            float: right;
+        }
+        .disc{
+       position: absolute;
+width: 45px;
+height: 45px;
+line-height: 15px;
+font-size: 15px;
+border-radius: 100%;
+z-index: 1;
+color: #fff;
+background: #B16704;
+            padding: 10px;
+font-weight: bold;
+        }
+        .zoom {
+    padding: 50px;
+    background-color: white;
+    transition: transform .2s; /* Animation */
+    width: 400px;
+    height: 400px;
+    margin: 0 auto;
+}
+
+.zoom:hover {
+    transform: scale(1.5); /* (150% zoom - Note: if the zoom is too large, it will go outside of the viewport) */
+}
+input[type=text], select, textarea {
+    width: 100%;
+    padding: 12px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    -webkit-box-sizing: border-box;
+            box-sizing: border-box;
+    margin-top: 6px;
+    margin-bottom: 16px;
+    resize: vertical;
+}
+        .curr{
+            background-image:  url('img/rs.png');
+            background-repeat: no-repeat;
+background-size: 87px .8em;
+-webkit-background-size: 87px .8em;
+display: inline-block;
+width: 9px;
+height: 1em;
+vertical-align: middle;
+            background-position: -39px 0;
+        }
+        .currs{
+            background-image:  url('img/rs.png');
+            background-repeat: no-repeat;
+background-size: 87px .8em;
+-webkit-background-size: 87px .8em;
+display: inline-block;
+width: 9px;
+height: 1em;
+vertical-align: middle;
+            background-position: 0 0;
+        }
+        .cut{
+            text-decoration: line-through !important;
+            font-size: 1.2rem !important;
+        }
+        .red{
+            color: #b16704;
+            font-size: 1.3rem !important;
+        }
+          .prc{
+            font-size: 1.2rem !important;
+        }
+        .blue{
+            color: #0066c0;
+             font-size: 1.4rem !important;
+            padding-left: 40px;
+        }
+        .pricers{
+            padding-left: 30px;
+        }
+        .pricersf{
+            padding-left: 33px;
+        }
+        .h2{
+            font-size: 1.5em !important;
+        }
+          .morecontent span {
+    display: none;
+}
+.morelink {
+    display: block;
+}
+        .specs{
+            padding-left: 16px;
+        }
+        .foot{
+            color: white;
+            font-size: 1.1em !important;
+            padding: 10px;
+            margin-bottom: 0px;
+        }
+        .navbar-inverse {
+    background-color: #050007 !important;
+	text-align: center;
+	font-size: 40px;
+}
+.fonts {
+    text-align: center;
+	font-size: 40px;
+}
+.container {
+    padding-right: 25px !important;
+    padding-left: 25px !important;
+}
+          .alert {
+    margin-top: -20px !important;
+padding: 0px;
+    background-color: #28343F;
+    color: white;
+              display: flex;
+}
+.custom66{
+    font-size: 17px!important;
+    line-height: 1.255!important;
+    font-family: Arial,sans-serif;
+       color: #008a00!important;
+}
+
+.closebtn {
+    margin-left: 15px;
+    color: white;
+    font-weight: bold;
+    float: right;
+    font-size: 30px;
+    line-height: 20px;
+    cursor: pointer;
+    transition: 0.S7 Edge;
+
+}
+
+.closebtn:hover {
+    color: black;
+}
+     .progress{
+            margin-bottom: 3px;
+            height: 12px;
+        }
+        .progress-bar{
+                background-color: green;
+            
+        }
+    </style>
+    <script>
+    $(document).ready(function() {
+    // Configure/customize these variables.
+    var showChar = 200;  // How many characters are shown by default
+    var ellipsestext = "...";
+    var moretext = "Show more >";
+    var lesstext = "Show less";
+    
+
+    $('.more').each(function() {
+        var content = $(this).html();
+ 
+        if(content.length > showChar) {
+ 
+            var c = content.substr(0, showChar);
+            var h = content.substr(showChar, content.length - showChar);
+ 
+            var html = c + '<span class="moreellipses">' + ellipsestext+ '&nbsp;</span><span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp;<a href="" class="morelink">' + moretext + '</a></span>';
+ 
+            $(this).html(html);
+        }
+ 
+    });
+ 
+    $(".morelink").click(function(){
+        if($(this).hasClass("less")) {
+            $(this).removeClass("less");
+            $(this).html(moretext);
+        } else {
+            $(this).addClass("less");
+            $(this).html(lesstext);
+        }
+        $(this).parent().prev().toggle();
+        $(this).prev().toggle();
+        return false;
+    });
+});
+</script>
+   <!-- Global site tag (gtag.js) - Google Analytics -->
+
+
+
+    </head>
+<body>
+<div class="container-fluid">
+    <nav class="navbar navbar-inverse">
+	<div class="fonts">
+	<center><font size="5" color="white"><b>Book Redmi Note 5 Pro Just In 699 Rs <b></font>
+	</div></center>
+	
+<center></center>
+</nav>
+        </div>
+   <center>
+
+ 
+</div>
+</center>
+    <div class="container">
+    <div class="row">
+    <div class="col-md-16">
+	
+        <a href="index.html"> <img src="img/logo.png" height="100px;" class="stars"></a>
+      
+        </div>
+    </div>
+<div class="row">
+        <div class="col-md-16"><b>
+Redmi Note 5 Pro</b>
+    </div>
+        </div>
+        <div class="row">
+        <div class="col-md-16"><br>
+            <p class="disc">95% <br>off</p>
+			<div class="zoom">
+        <br>   <center> <img src="img/1.jpg" class="img-responsive" style="width: 300px;"><br>
+		<p><font size="6"><b>Redmi Note 5 Pro</b></font></p></center>
+            </div>
+        </div>      <br>   <br> 
+        <div class="row">
+        <div class="col-md-16">
+             
+             
+            
+            <p class="size">
+            Colour: <select id="amount" name="Colour">
+      <option value="Black">Black</option>
+	  <option value="Gold">Gold</option>
+	  <option value="Blue">Blue</option>
+	  <option value="Pink">Pink</option>
+     </select>
+            </div>
+        </div>
+   <div class="row">
+        <div class="col-md-16">
+            
+            
+            <p class="size">
+            Size: <br> <select id="amount" name="Colour">
+			<option value="Black">64GB</option>
+	 </select>
+			
+                
+            </p>
+            
+            </div>
+        </div>
+           <div class="row pricers">
+      
+        </div>
+<center>
+
+
+</center><br>
+           <div class="row pricersf">
+        <div class="col-md-16" style="    margin-left: -30px;">
+           <span class="red"></span><span class="currs" style="    margin-top: -40px;"></span> <span class="red" style="font-size: 3.8rem !important;">699</span> &nbsp; <img src="img/f.png" height="25" width="51" style="    margin-top: -42px;">
+               </div>
+  <div class="col-md-16">
+          <span class="prc">  Price:</span> <span class="curr"></span> <span class="cut">13999</span><br><center><span class="prc">Inclusive of all taxes</span></center>
+               </div>
+        </div>
+<br>
+        <div class="container-fluid">
+  <div class="progress">
+    <div class="progress-bar" role="progressbar" aria-valuenow="46" aria-valuemin="0" aria-valuemax="100" style="width:80%">
+      <span class="sr-only">80% Sold</span>
+    </div>
+  </div>
+            <center><span style="text-align: center; font-size: 15px !important; color: red;"><b>Deal is 80% Sold.</b></span></center>
+            
+</div>
+ <div class="row">
+        <div class="col-md-16"><br>
+           
+		   <br><center> <a href="add2cart.html"><img src="img/add.png" height="200" width="300" class="img-responsive"></a> </center> 
+            </div>
+        </div> <br>
+        <center>
+            <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- Anshu offer 2 -->
+<ins class="adsbygoogle"
+     style="display:inline-block;width:300px;height:250px"
+     data-ad-client="ca-pub-5774183160291534"
+     data-ad-slot="6377615750"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+        </center>
+           <div class="row pricers">
+        <div class="col-md-16">
+        <br>    <span class="blue">Free Delivery</span><br>
+               </div>
+        </div>
+         <div class="row">
+        <div class="col-md-16">
+            <span class="prc"> You Save: </span> <span class="currs"></span> <span class="red">12300 (95%)</span><br>  <span class="prc"> <b>Cash on Delivery</b> eligible.<br>	
+
+
+          
+            
+
+<b style="color: green; font-size: 18px !important;">In stock</b>.<br><br>
+<center>
+
+
+</center>
+            Delivery to your Address In<b><font color="green"> 7-9 Days</b></font> 
+            </span>
+             </div>
+        </div>
+       
+         <div class="row">
+        <div class="col-md-16"><br>
+            <img src="img/limit.png" class="img-responsive pull-left">  The order quantity for this product is limited to 1 unit per customer<br>
+
+Please note that orders which exceed the quantity limit will be auto-canceled. This is applicable across sellers. 
+             </div>
+        </div>
+          <div class="row">
+		  
+<center><img src="img/rating.png" class="img-responsive"></center>
+        <div class="col-md-16"><br>
+         
+            <h2 class="h2">
+Technical Specifications</h2>
+            
+            
+             <table class="table">
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Description</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>Brand</td>
+        <td>Redmi</td>
+
+      </tr>
+      <tr>
+        <td>Colour</td>
+        <td>Black</td>
+        
+      </tr>
+      <tr>
+        <td>Item Weight</td>
+        <td>157 g</td>
+  
+      </tr>
+            <tr>
+        <td>Item model number</td>
+        <td>Redmi Note 5 Pro </td>
+
+      </tr>
+      <tr>
+        <td>Size</td>
+        <td>64GB</td>
+        
+      </tr>
+      <tr>
+        <td>RAM</td>
+        <td>4GB</td>
+  
+      </tr>
+            <tr>
+        <td>Operating System</td>
+        <td>Android 6.0 Marshmallow</td>
+
+      </tr>
+      <tr>
+        <td>Battery</td>
+        <td>3000mAh</td>
+        
+      </tr>
+    
+    </tbody>
+  </table>
+            
+            
+            
+            <h2 class="h2">
+Product Description</h2>
+        <span class="more">
+The beauty of what we've engineered is to give you the slimmest feel in your hand without compromising the big screen size. And the elegantly curved front and back fit in your palm just right. It's as beautiful to look at, as it is to use.
+            <h2 class="h2">
+From the Manufacturer</h2>
+            <b>Camera</b>
+    Dual Pixel 16MP, Bright Lens F1.7, 1.4um pixels on image sensor with Phase Detection Action Autofocus, Motion Panorama, Motion Photo and Hyperlapse.
+            <b>Gaming Features</b>
+          Mobile gaming just got a whole lot better with a dedicated game control center and gameplay recording. And with the Moto G4 Plus 's powerful processor, it just got highly realistic.
+            <br>
+            <ul class="specs">
+    <li> ROM: 64GB</li>c
+    <li> Ram: 4GB</li>
+    <li> Fingerprint: Enabled</li>
+            </ul>
+    </span>
+              </div>
+        </div>
+        </div>
+        What is cryptocurrency trading <br><br>
+The main question arises in most people’s mind is what is cryptocurrency? So, cryptocurrency is the digital asset designed to work as a medium to exchange that uses cryptography to secure is transactions. It is form of decentralized digital and electronic money. The first cryptocurrency is the bitcoin and after that many cryptocurrencies came into play such as Litecoin, Ethereum, Zcash, Dash, Ripple, Monero, the Bottom line etc. Cryptocurrency is called the new gold.<br>
+So, as we now know what cryptocurrency is, let’s know about Cryptocurrency trading. Cryptocurrency trading is also known as cryptocurrency exchange or Digital currency exchanges (DCE). It is the trading in which businesses that allows customers to trade cryptocurrencies or digital currencies for other assets, such as conventional flat money or different digital currency. There are also altcoins (altcoins are the cryptos that are not bitcoins). In cryptocurrency trading, altcoins can be paired with bitcoins. <br>
+Cryptocurrency trading is just and alternative way to get involved in the crypto-world. Cryptocurrency trading doesn’t require mining hardware nor investing in bitcoin hyips or bitcoin cloud mining. 
+To start cryptocurrency trading, you first of all need a wallet, without wallet you cannot buy any cryptocurrency. The majority of the Cryptocurrency trading or exchanges have a free wallet along with the ability to trade. But in cryptocurrency trading, the users are suggested not to keep all their cryptos at one place because they can get hacked or scammed. <br><br>
+You must be familiar with an idiom saying: “Trade the rumor- sell the news.
+This is an old saying in the share exchange world but it is also valid in cryptocurrency trading.
+There are some golden rules which has to be followed in the cryptocurrency trading world.<br><br>
+Always do your homework and read the fundamentals of a coin. If you read the whitepapers, you will know more than 90% of the people we know trade cryptos.<br>
+Go to crypto events. That is the place you will find the hottest coins!<br>
+Buy weeks before a roadmap (of development) gets revealed. It is like the stock earning releases – something gets so hyped-up that even a good report can’t help it.<br>
+Never buy into a coin after a dump.<br>
+Learn to understand the Block (or any) -chain.<br>
+Learn to understand the correlation between Bitcoin and an altcoin.<br>
+Learn to understand candlestick charts. They offer significant information, which can’t be figured out of normal lined charts.<br>
+Secure winning & limit losses<br>
+Learn technical analysis. This plays a major role in reading current market movement.<br>
+Don’t trade if you lose your sleep because of a single trade. That is just not worth it.<br>
+ 
+So this is all about Cryptocurrency trading. 
+
+        <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-16">
+                <nav class="navbar navbar-inverse foot">
+               <center> This Offer Is Valid Only For Limited Time.</center>
+                </nav>
+				
+        </div>
+            </div>
+            </div>
+    </body>
