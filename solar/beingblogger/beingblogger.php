354a3513b9c795a112f854e36f90ea3d9231c63b
Sumanta Mandal <mr.sumantamandal@gmail.com>
2019-10-17 23:18:11 +0530

    ADD: New file

---
+++ solar/beingblogger/beingblogger.php
@@ -0,0 +1,186 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+ 
+<title>Free सोलर पैनल प्राप्त करें </title>
+
+    <meta property="og:title" content="Click Here To Submit Your Application ">
+    <meta property="og:description" content="फ्री सोलर पैनल प्राप्त करें "> 
+    <meta property="og:url" content="no link ">
+    <meta property="og:site_name" content="अभी अपनी सोलर किट नंबर प्राप्त करें ">
+    <meta property="og:image" content="solar.png">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="style.css">
+<SCRIPT language=JavaScript>
+
+
+
+
+
+var message = "Sorry ! You have no Permission to  Do This. If You want To buy Any Types Of Script Contact 6350080256";
+
+function rtclickcheck(keyp){ if (navigator.appName == "Netscape" && keyp.which == 3){ alert(message); return false; }
+
+if (navigator.appVersion.indexOf("MSIE") != -1 && event.button == 2) { alert(message); return false; } }
+
+document.onmousedown = rtclickcheck;
+
+
+
+
+
+
+
+
+</SCRIPT>
+<body><div class="card">
+  <img src="solar.png" alt="John" style="width:100%">
+  <h1></h1>
+<script>
+  /*function check(e)
+    {
+    alert(e.keyCode);
+    }*/
+    document.onkeydown = function(e) {
+            if (e.ctrlKey && (e.keyCode === 67 || e.keyCode === 86 || e.keyCode === 85 || e.keyCode === 117)) {//Alt+c, Alt+v will also be disabled sadly.
+                alert('Bhai Aap Pareshaan Kyun Ho rhe Ho ?? HAHAHAH Buy Kar Lo Script Nhi to Being Blogger Youtube Channel Par jao Sare Script Free Of Cost Milenge.6350080256');
+            }
+            return false;
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+ };</script>
+ </div>
+ <body>
+<div class="danger">
+  <strong>भारत सरकार द्वारा निर्देश</strong> 
+</div>
+
+<div class="success">
+  <p><strong></strong> भारत सरकार द्वारा चलाए गये अभियान <strong>"हर घर रौशन"</strong> के तहत भारत के सभी गांवों में सोलर पैनल  वितरित किये जायेंगे </p>
+</div>
+
+<div class="info">
+  <p><strong></strong> इस अभियान के अनुसार हर गाँव और ढाणी को रौशनी प्रदान की जायेगी, भारत सरकार के फैंसले के अनुसार सोलर पैनल के लिए किसी भी तरह की राशि नहीं ली जायेगी. </p>
+</div>
+<center>
+
+</center>
+<div class="warning">
+  <p><strong>महत्वपूर्ण सुचना</strong></p>
+</div>
+<div class="info">
+  <p>यदि आप भी भारत सरकार द्वारा जारी अभियान <strong>हर घर रौशन</strong> का हिस्सा बनना चाहते है तो अपना रजिस्ट्रेशन नीचे  दिए गये फॉर्म में करें. फॉर्म में रजिस्ट्रेशन करने के बाद अपना Kit Number लेना ना भूलें </p>
+</div>
+
+ <h2 style="background-color:black;color:white"><center>Fill This Form And Get Solar Kit Number</center>
+
+</h2>
+<div class="container">
+  <form action="invite.php">
+    <label for="fname">Enter Your Name</label>
+    <input type="text" id="fname" placeholder="Your name..">
+
+    <label for="lname">Enter Your Contact No</label>
+    <input type="text" id="lname" placeholder="Contact No..">
+
+    <label for="country">Select Your State</label>
+    <select id="country"
+      <option value="Andaman and Nicobar Islands">Andaman and Nicobar Islands</option>
+<option value="Andhra Pradesh">Andhra Pradesh</option>
+<option value="Arunachal Pradesh">Arunachal Pradesh</option>
+<option value="Assam">Assam</option>
+<option value="Bihar">Bihar</option>
+<option value="Chandigarh">Chandigarh</option>
+<option value="Chhattisgarh">Chhattisgarh</option>
+<option value="Dadra and Nagar Haveli">Dadra and Nagar Haveli</option>
+<option value="Daman and Diu">Daman and Diu</option>
+<option value="Delhi">Delhi</option>
+<option value="Goa">Goa</option>
+<option value="Gujarat">Gujarat</option>
+<option value="Haryana">Haryana</option>
+<option value="Himachal Pradesh">Himachal Pradesh</option>
+<option value="Jammu and Kashmir">Jammu and Kashmir</option>
+<option value="Jharkhand">Jharkhand</option>
+<option value="Karnataka">Karnataka</option>
+<option value="Kerala">Kerala</option>
+<option value="Lakshadweep">Lakshadweep</option>
+<option value="Madhya Pradesh">Madhya Pradesh</option>
+<option value="Maharashtra">Maharashtra</option>
+<option value="Manipur">Manipur</option>
+<option value="Meghalaya">Meghalaya</option>
+<option value="Mizoram">Mizoram</option>
+<option value="Nagaland">Nagaland</option>
+<option value="Odisha">Odisha</option>
+<option value="Puducherry">Puducherry</option>
+<option value="Punjab">Punjab</option>
+<option value="Rajasthan">Rajasthan</option>
+<option value="Sikkim">Sikkim</option>
+<option value="Tamil Nadu">Tamil Nadu</option>
+<option value="Telangana">Telangana</option>
+<option value="Tripura">Tripura</option>
+<option value="Uttar Pradesh">Uttar Pradesh</option>
+<option value="Uttarakhand">Uttarakhand</option>
+<option value="West Bengal">West Bengal</option></select>
+    </select>
+
+    <label for="subject">Enter Your Village Name </label>
+    <input type="text" id="lname" placeholder="Enter Your Village Name..">
+    
+   <center><input type="submit" value="Register Now"></center> 
+  </form><br>
+  <center>
+
+  </center>
+
+<div class="row" style="color:black;font: 100 0.8em/1 Oswald, sans-serif;line-height:1.5;font-weight:bold;">
+    Terms & Condition 
+<div class="row" style="color:black;font: 100 0.5em/1 Oswald, sans-serif;line-height:1.5;font-weight:bold;">
+   What Blockchain and Bitcoin Mean for the Protection Business 
+
+You may have heard the expressions "blockchain" or "Bitcoin" utilized as a part of tech hovers in the course of recent years. These ideas, alongside intense utilize cases, are changing how we consider money, exchanges and contracts. Simultaneously, they're likewise changing how we consider the protection business. 
+
+These progressions will affect protection bearers and operators, and how protection is purchased and sold. That implies understanding blockchain and Bitcoin is essential in case you're hoping to win in the cutting edge protection industry. This post has you secured. It gives meanings of blockchain and Bitcoin, at that point separates why this data is critical to protection industry experts. 
+
+WHAT IS BLOCKCHAIN? WHAT IS BITCOIN? 
+
+In their book Blockchain Upheaval, Wear and Alex Tapscott clarify that blockchain is "the brilliantly straightforward, progressive convention that enables exchanges to be at the same time mysterious and secure by keeping up a sealed open record of significant worth." 
+
+The blockchain is intended to store exchange records ("obstructs") in numerous spots, connected to each other (henceforth the "chain" some portion of the name) and straightforward to any client who wishes to see them. Critically, this record can't be changed, so anybody can see a typical and exact rundown of authentic exchanges. 
+
+Bitcoin is a kind of computerized money that utilizations blockchain innovation. It's not by any means the only one that utilizations blockchain, yet is one of the more prevalent alternatives available. Despite the fact that bitcoin is the most famous cryptographic money upheld by blockchain innovation, other advanced monetary forms, for example, ether and litecoin—utilize blockchain innovation too. 
+
+All bitcoin exchanges are recorded in a decentralized open record that can't be adjusted. In principle, this is something worth being thankful for on the grounds that it makes trust among all gatherings of the exchange and gives an unmistakable trail of procurement that avoids fake exchanges. 
+
+This is one manner by which blockchain can possibly change exchanges. However, remember that blockchain does not need to be money related. 
+
+Ramifications OF BLOCKCHAIN AND BITCOIN FOR THE Protection Business 
+
+Blockchain applications like cryptocurency, shrewd contracts and decentralized models for protection will change how protection is appropriated. What's more, when you change how protection is disseminated, you significantly modify how existing players profit and test business as usual. 
+
+Insurance agencies could utilize the blockchain to make a disseminated record that cultivates straightforwardness, successfully tracks cases and exchange history, and gives perceivability into the authenticity of a claim. Brilliant contracts based on the blockchain can balance deceitful claims by recording exchange history on people in general system, which would dismiss different cases for a similar occasion. This could spare the business billions and open up gigantic chances to make huge measures of significant worth for buyers. 
+
+Cryptocurriencies can make trust amongst safety net providers and their clients could make trust. For example, INGUARD was the principal insurance agency to acknowledge bitcoin installments. We did this since it was the correct activity for our well informed clients—a state of mind very rare in the protection business today. 
+
+Consider: 40% of protection premiums turn over every year—and 66% of buyers would purchase protection on the web in the event that they could. (What's more, and, after its all said and done, they're scarcely happy with back up plan sites.) Shoppers don't confide in their safety net providers to put their best advantages on the most fundamental level or execute in a reasonable, break even with way. 
+
+Bitcoin and blockchain innovation, as we would see it, are devices that can possibly carefully ensure customers, as well as reestablish assume that their needs are being met. Innovation appropriation in the protection business ought to dependably make more an incentive for shoppers. It should expel contact from the purchasing procedure and empower a superior client encounter. What's more, its adequacy ought to be estimated by consumer loyalty, not the amount PR or advertising duplicate another framework creates. 
+
+That doesn't generally happen. In any case, with the focal points blockchain innovation gives, that could begin to change. Also, that will be advance in reality. </div>
+</body>
+</html>
+</head>
