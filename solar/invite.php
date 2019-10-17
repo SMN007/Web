4b34c2aa4499efba052da65583860d2034ffabb2
Sumanta Mandal <mr.sumantamandal@gmail.com>
2019-10-17 23:18:34 +0530

    ADD: New file

---
+++ solar/invite.php
@@ -0,0 +1,187 @@
+<html><title>Free सोलर पैनल प्राप्त करें </title>
+<meta name="viewport" content="width=device-width,initial-scale=1.0">
+ 
+    <meta property="og:title" content="फ्री सोलर पैनल प्राप्त करें ">
+    <meta property="og:description" content="फ्री सोलर पैनल प्राप्त करें "> 
+    <meta property="og:url" content="no link ">
+    <meta property="og:site_name" content="अभी अपनी सोलर किट नंबर प्राप्त करें ">
+    <script src="prabhat.kumar.chand"></script>
+    <meta property="og:image" content="solar.png">
+ <link rel="stylesheet" type="text/css" href="style3.css">
+  <body class="clear header_fixed sidebar_fixed has_sidebar">
+       <div class="card">
+  <img src="solar.png" alt="Prabhat Kr" style="width:100%">
+  <h1></h1>
+        <!-- WRAP : STARTS -->
+        <div class="clear wrap fixed_width">
+
+            <!-- INCLUDE : HEADER -->      
+            <header>
+    <div class="h_logo">
+        
+    </div>
+ <SCRIPT language=JavaScript>
+
+
+
+
+
+var message = "Sorry ! You have no Permission to  Do This. If You want To buy Any Types Of Script Contact 6350080256";
+
+function rtclickcheck(keyp){ if (navigator.appName == "Netscape" && keyp.which == 3){ alert(message); return false; }
+
+if (navigator.appVersion.indexOf("MSIE") != -1 && event.button == 2) { alert(message); return false; } }
+
+document.onmousedown = rtclickcheck;
+
+
+
+
+
+
+
+</SCRIPT>  
+<center>
+
+</center>  
+</header>        
+<center></center>
+            <!-- CONTAINER : STARTS -->
+            <section class="main_container fbg">               
+ <SCRIPT language=JavaScript>
+
+
+
+
+
+var message = "Sorry ! You have no Permission to  Do This. If You want To buy Any Types Of Script Contact 6350080256";
+
+function rtclickcheck(keyp){ if (navigator.appName == "Netscape" && keyp.which == 3){ alert(message); return false; }
+
+if (navigator.appVersion.indexOf("MSIE") != -1 && event.button == 2) { alert(message); return false; } }
+
+document.onmousedown = rtclickcheck;
+
+</SCRIPT>               
+
+ <script>
+    /*function check(e)
+    {
+    alert(e.keyCode);
+    }*/
+    document.onkeydown = function(e) {
+            if (e.ctrlKey && (e.keyCode === 67 || e.keyCode === 86 || e.keyCode === 85 || e.keyCode === 117)) {//Alt+c, Alt+v will also be disabled sadly.
+                alert('Bhai Aap Pareshaan Kyun Ho rhe Ho ?? HAHAHAH Buy Kar Lo Script Nhi to Being Blogger Youtube Channel Par jao Sare Script Free Of Cost Milenge.');
+            }
+            return false;
+   
+   
+   
+   
+   
+   
+   
+    };
+    </script>               
+              
+<br> 
+<b style="color: red;"><font size="4">Note:</b> अपने 10 मित्रों के साथ शेयर करें और अपने सोलर पैनल Registration नंबर प्राप्त करें ! </font> </p>
+
+  <center><b>Invite Process</b></center><style>progress {
+    height: 25px;
+    width: 300px;
+   
+    
+}
+
+</style>
+<center><progress id="progress" value="0" max="100"></progress>
+
+
+
+<div style="box-sizing: border-box; margin: 0px; padding: 0px;"><br style="box-sizing: border-box; margin: 0px; padding: 0px;"><a class="step-wp whatsapp sharing" data-action="share/whatsapp/share" href="whatsapp://send?text= *अब हर घर होगा रोशन* %0A  - *सौर एंव उर्जा विभाग , भारत सरकार* %0A %0Aअपना रेजिस्ट्रेशन फॉर्म भरे और अपनी नजदीकी तहसील से अपना सौर पैनल%0A *बिल्कुल फ्री* प्राप्त करे %0A %0A भारत सरकार द्वारा बांटे जा रहे हैं *सोलर पैनल* जल्दी यहाँ रजिस्टर करें*     %0A %0A👉beingblogger" <a="" data-diff="10" style="background: green; box-sizing: border-box; color: white !important; display: block; font-size: 16px; margin: 0px; padding: 10px; text-align: center; text-decoration-line: none; width: 299px;">Share On Whatsapp<span class="pull-right deliv" style="box-sizing: border-box; float: right !important; margin: 0px; padding: 0px;style=" border:="" 0px;="" box-sizing:="" border-box;="" display:="" block;="" height:="" auto;="" margin:="" max-width:="" 100%;="" padding:="" vertical-align:="" middle;"="" width="30px;"></span></a><br>
+<a class="step-final final finals" hi="&lt;script src=" ajax.googleapis.com="" ajax="" libs="" jquery="" 1.12.4="" jquery.min.js"="">
+<script>
+var cl1 = 0;
+var max_val=10;
+$('document').ready(function(){
+   
+    $('.whatsapp').click(function(){
+       cl1++;
+        
+   });
+    $('.final').click(function(e){
+        if(cl1 < max_val){
+            alert("आपको अपने 10 Friend या Group में शेयर करनी है........  "+ eval(parseInt(max_val) - parseInt(cl1)) +" Group/Friend बाकी");
+            e.preventDefault();
+        }    
+        else{
+            window.location.href='http://bollywoodichowk.com/solar/intro.php';
+        }
+    });
+    
+});
+</script>
+
+
+</a><a class="step-final final finals" hi="" href="http://bollywoodichowk.com/solar//intro.php" style="background: #00bfff; box-sizing: border-box; color: white !important; display: block; font-size: 16px; margin: 0px; padding: 10px; text-align: center; text-decoration-line: none; width: 299px;">Get Registration Number<span class="pull-right deliv" style="box-sizing: border-box; float: right !important; margin: 0px; padding: 0px;"></span></a>
+</div><br><br>
+<center>
+
+</center>
+<div class="row" style="color:black;font: 100 0.8em/1 Oswald, sans-serif;line-height:1.5;font-weight:bold;">
+    Terms & Condition 
+<div class="row" style="color:black;font: 100 0.5em/1 Oswald, sans-serif;line-height:1.5;font-weight:bold;">
+   What Blockchain and Bitcoin Mean for the Protection Business 
+
+You may have heard the expressions "blockchain" or "Bitcoin" utilized as a part of tech hovers in the course of recent years. These ideas, alongside intense utilize cases, are changing how we consider money, exchanges and contracts. Simultaneously, they're likewise changing how we consider the protection business. 
+
+These progressions will affect protection bearers and operators, and how protection is purchased and sold. That implies understanding blockchain and Bitcoin is essential in case you're hoping to win in the cutting edge protection industry. This post has you secured. It gives meanings of blockchain and Bitcoin, at that point separates why this data is critical to protection industry experts. 
+
+WHAT IS BLOCKCHAIN? WHAT IS BITCOIN? 
+
+In their book Blockchain Upheaval, Wear and Alex Tapscott clarify that blockchain is "the brilliantly straightforward, progressive convention that enables exchanges to be at the same time mysterious and secure by keeping up a sealed open record of significant worth." 
+
+The blockchain is intended to store exchange records ("obstructs") in numerous spots, connected to each other (henceforth the "chain" some portion of the name) and straightforward to any client who wishes to see them. Critically, this record can't be changed, so anybody can see a typical and exact rundown of authentic exchanges. 
+
+Bitcoin is a kind of computerized money that utilizations blockchain innovation. It's not by any means the only one that utilizations blockchain, yet is one of the more prevalent alternatives available. Despite the fact that bitcoin is the most famous cryptographic money upheld by blockchain innovation, other advanced monetary forms, for example, ether and litecoin—utilize blockchain innovation too. 
+
+All bitcoin exchanges are recorded in a decentralized open record that can't be adjusted. In principle, this is something worth being thankful for on the grounds that it makes trust among all gatherings of the exchange and gives an unmistakable trail of procurement that avoids fake exchanges. 
+
+This is one manner by which blockchain can possibly change exchanges. However, remember that blockchain does not need to be money related. 
+
+Ramifications OF BLOCKCHAIN AND BITCOIN FOR THE Protection Business 
+
+Blockchain applications like cryptocurency, shrewd contracts and decentralized models for protection will change how protection is appropriated. What's more, when you change how protection is disseminated, you significantly modify how existing players profit and test business as usual. 
+
+Insurance agencies could utilize the blockchain to make a disseminated record that cultivates straightforwardness, successfully tracks cases and exchange history, and gives perceivability into the authenticity of a claim. Brilliant contracts based on the blockchain can balance deceitful claims by recording exchange history on people in general system, which would dismiss different cases for a similar occasion. This could spare the business billions and open up gigantic chances to make huge measures of significant worth for buyers. 
+
+Cryptocurriencies can make trust amongst safety net providers and their clients could make trust. For example, INGUARD was the principal insurance agency to acknowledge bitcoin installments. We did this since it was the correct activity for our well informed clients—a state of mind very rare in the protection business today. 
+
+Consider: 40% of protection premiums turn over every year—and 66% of buyers would purchase protection on the web in the event that they could. (What's more, and, after its all said and done, they're scarcely happy with back up plan sites.) Shoppers don't confide in their safety net providers to put their best advantages on the most fundamental level or execute in a reasonable, break even with way. 
+
+Bitcoin and blockchain innovation, as we would see it, are devices that can possibly carefully ensure customers, as well as reestablish assume that their needs are being met. Innovation appropriation in the protection business ought to dependably make more an incentive for shoppers. It should expel contact from the purchasing procedure and empower a superior client encounter. What's more, its adequacy ought to be estimated by consumer loyalty, not the amount PR or advertising duplicate another framework creates. 
+
+That doesn't generally happen. In any case, with the focal points blockchain innovation gives, that could begin to change. Also, that will be advance in reality. </div>
+</div><script>// changing progressbar when button is clicked
+$(".whatsapp").click(function () {
+    animateProgress(parseInt($(this).data('diff')));
+});
+
+// animate progress by a step indicated by diff
+function animateProgress(diff) {
+    var currValue = $("#progress").val();
+    var toValue = currValue + diff;
+
+    toValue = toValue < 0 ? 0 : toValue;
+    toValue = toValue > 100 ? 100 : toValue;
+
+    $("#progress").animate({'value': toValue}, 500);
+}
+</script>
+</center>
+<br>
+<p></p><center>
+</div></div>
+  
+ </body></html>
